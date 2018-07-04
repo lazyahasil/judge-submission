eedb5eeb08c766031da9d03667dc12ab959b8201
Ahasil <lazyahasil+dev@gmail.com>
2018-07-04 18:03:48 +0900

    Added 02839 Sugar delivery in C++

---
+++ baekjoon/dp-novice/02839-sugar-delivery.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> opt_counts;
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    opt_counts.resize(n + 1, -1);
+
+    opt_counts[3] = 1;
+    opt_counts[5] = 1;
+
+    for (int i = 6; i <= n; i++) {
+        if (opt_counts[i - 3] != -1) {
+            if (opt_counts[i - 5] != -1) {
+                opt_counts[i] = min(opt_counts[i - 3], opt_counts[i - 5]) + 1;
+            } else {
+                opt_counts[i] = opt_counts[i - 3] + 1;
+            }
+        } else if (opt_counts[i - 5] != -1) {
+            opt_counts[i] = opt_counts[i - 5] + 1;
+        }
+    }
+
+    printf("%d\n", opt_counts[n]);
+
+    return 0;
+}
